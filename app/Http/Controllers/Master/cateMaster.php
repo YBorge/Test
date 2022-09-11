1f0c5a9a70ae6f252caa1d30e32c33837a963bcc
YBorge <36295790+YBorge@users.noreply.github.com>
2022-09-11 18:03:49 +0530

    Create cateMaster.php

--- app/Http/Controllers/Master/cateMaster.php
+++ app/Http/Controllers/Master/cateMaster.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Master;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\common_list_master;
+use App\Models\category_master;
 
 class cateMaster extends Controller
 {
     public function index()
     {
-        return view('master.cate_master');
+    	$cat_mater = category_master::where('status', '=', 'Y')
+                            ->pluck('cat_name','cat_code');
+    	$food_type = common_list_master::where('status', '=', 'Y')
+                            ->where('list_code', '=', 'CAT_TYPE')
+                            ->pluck('list_value','list_id');
+        return view('master.cate_master',['food_type' => $food_type,'cat_mater' => $cat_mater]);
     }
 }
