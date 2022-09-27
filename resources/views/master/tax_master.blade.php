d9021978818ad12ca1c6f94f084e59b1b3eb7d86
YBorge <36295790+YBorge@users.noreply.github.com>
2022-09-27 23:33:47 +0530

    list

--- resources/views/master/tax_master.blade.php
+++ resources/views/master/tax_master.blade.php
@@ -80,25 +80,19 @@
         <table class="mytable table table-bordered" id="example" class="display nowrap" style="width:100%">
             <thead style="position: sticky;top: 0" class="thead-dark">
                 <tr>
-                    <th class="header" scope="col">Edit</th>
+                    
                     <th class="header" scope="col">Sr. No</th>
-                    <th class="header" scope="col">Code</th>
-                    <th class="header" scope="col">Name</th>
                     <th class="header" scope="col">Type</th>
-                    <th class="header" scope="col">Credit Days</th>
-                    <th class="header" scope="col">Address 1</th>
-                    <th class="header" scope="col">Address 2</th>
-                    <th class="header" scope="col">City</th>
-                    <th class="header" scope="col">State</th>
-                    <th class="header" scope="col">Country</th>
-                    <th class="header" scope="col">Pin Code</th>
-                    <th class="header" scope="col">Phone No</th>
-                    <th class="header" scope="col">Email</th>
-                    <th class="header" scope="col">GSTIN</th>
-                    <th class="header" scope="col">FASSI No</th>
-                    <th class="header" scope="col">Aadhar No</th>
-                    <th class="header" scope="col">Pan No</th>
-                    <th class="header" scope="col">Contact Person</th>
+                    <th class="header" scope="col">Name</th>
+                    <th class="header" scope="col">Code</th>
+                    <th class="header" scope="col">Tax (%)</th>
+                    <th class="header" scope="col">Tax Indicator</th>
+                    <th class="header" scope="col">IGST (%)</th>
+                    <th class="header" scope="col">SGST (%)</th>
+                    <th class="header" scope="col">CGST (%)</th>
+                    <th class="header" scope="col">UTGST (%)</th>
+                    <th class="header" scope="col">CESS (%)</th>
+                    <th class="header" scope="col">Cess Per Peice (%)</th>
                     <th class="header" scope="col">Status</th>
                     <th class="header" scope="col">Created By</th>
                     <th class="header" scope="col">Created Date and Time</th>
@@ -106,7 +100,28 @@
                     <th class="header" scope="col">Updated Date and Time</th>
                 </tr>
             </thead>
-                 
+                @php $srNo=0; $arrOfType=array(); $arrOfType['G']='GST'; $arrOfType['V']='Vat'; $arrOfStatus=array(); $arrOfStatus['Y']='Active'; $arrOfStatus['N']='In-Active'; @endphp
+                @foreach($tax_master_data as $taxKey => $taxValue)
+                <tr>
+                    <td>{{++$srNo}}</td>
+                    <td>{{$arrOfType[$taxValue->tax_type]}}</td>
+                    <td>{{$taxValue->tax_code}}</td>
+                    <td>{{$taxValue->tax_name}}</td>
+                    <td>{{$taxValue->tax_per}}</td>
+                    <td>{{$taxValue->tax_indicator}}</td>
+                    <td>{{$taxValue->igst}}</td>
+                    <td>{{$taxValue->sgst}}</td>
+                    <td>{{$taxValue->cgst}}</td>
+                    <td>{{$taxValue->utgst}}</td>
+                    <td>{{$taxValue->cess}}</td>
+                    <td>{{$taxValue->cessperpiece}}</td>
+                    <td>{{$arrOfStatus[$taxValue->status]}}</td>
+                    <td>{{$taxValue->created_by}}</td>
+                    <td>{{$taxValue->created_at}}</td>
+                    <td>{{$taxValue->t_user}}</td>
+                    <td>{{$taxValue->updated_at}}</td>
+                </tr>
+                @endforeach
             </table>                   
         </div>  
     </div>
