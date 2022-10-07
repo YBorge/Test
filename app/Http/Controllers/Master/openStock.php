b90cc826f267648b07bf068ecc1a4ca8202f1b2d
YBorge <36295790+YBorge@users.noreply.github.com>
2022-10-07 15:07:11 +0530

    open stock

--- app/Http/Controllers/Master/openStock.php
+++ app/Http/Controllers/Master/openStock.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Master;
-use Session;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -11,6 +11,8 @@ use App\Models\dept_master;
 use App\Models\item_barcode;
 use App\Models\item_master;
 use App\Models\stock_detail;
+
+use Session;
 use Response;
 use PDF;
 use Excel;
@@ -19,6 +21,7 @@ class openStock extends Controller
 {
     public $dept_code;
     public $open_stockdata;
+   
     public function __construct()
     {
         $this->dept_code = dept_master::where('status', '=', 'Y')->pluck('dept_name','dept_code');
@@ -26,7 +29,6 @@ class openStock extends Controller
     }
     public function index()
     {
-        
         return view('master.openStock',['open_stockdata'  => $this->open_stockdata,'dept_code' => $this->dept_code]);
     }
 
@@ -56,39 +58,42 @@ class openStock extends Controller
         {
             return Response::json(['errors' => $validatedData->errors()->first()]);
         }
-        $open_stock_count = DB::table('open_stock')->where('item_code', '=', $request->item_code)->count();
-        
+        $open_stock_count = DB::table('open_stock')->where('item_code', '=', $request->item_code)->count(); // get count 
+       
         $batch_no_get = DB::table('open_stock')
-            ->select('batch_no')
-            ->where('item_code', '=' , $request->item_code)
-            ->first();
-        
-            if($request->item_type='V')
+                        ->select('batch_no')
+                        ->where('item_code', '=' , $request->item_code)
+                        ->first();
+        $max_batch_code=open_stock::where('item_code', '=' , $request->item_code)->max('batch_no');
+        $min_batch_code=open_stock::where('item_code', '=' , $request->item_code)->min('batch_no');
+    
+        if($request->item_type='V')
+        {
+            if($max_batch_code=='')
             {
-                if(!$batch_no_get)
-                {
-                  $batch_post=-99; 
-                }
-                else{
-                   $batch_post=null; 
-                }
+                $batch_post=-99; 
             }
-            elseif($request->item_type='P' or $request->item_type='L')
+            elseif($max_batch_code <> '')
             {
-                if(!$batch_no_get)
-                {
-                  $batch_post=-1; 
-                }
-                else{
-                    $batch_post=null; 
-                 }
+                $batch_post=1+$max_batch_code;
             }
+        }
+        elseif($request->item_type='P' or $request->item_type='L')
+        {
+            if($min_batch_code=='')
+            {
+                $batch_post=-1; 
+            }
+            elseif($min_batch_code <> '')
+            {
+                $batch_post=$min_batch_code - 1; 
+            }
+        }
         
         if ($validatedData->passes()) 
         {
             if($open_stock_count==0)
             {
-            
                 open_stock::create([
                     'loc_Code' => Session::get('companyloc_code'),
                     'barcode' => $request->barcode,
@@ -100,18 +105,71 @@ class openStock extends Controller
                     'dept_code' => $request->dept_code,
                     'expiry_date' => $request->expiry_date,
                     'batch_no' => $batch_post,
-                    'doc_type' => '',
+                    'doc_type' => 'OB',
                     'comp_code' => Session::get('companycode'),
                     'status' => 'Y',
                     'created_by' => Session::get('useremail')
                 ]);
+
+                stock_detail::create([
+                    'loc_Code' => Session::get('companyloc_code'),
+                    'batch_no' => $batch_post,
+                    'item_code' => $request->item_code,
+                    'dept_code' => $request->dept_code,
+                    'doc_type' => 'OB',
+                    'recd_date' => null,
+                    'recd_qty' => $request->qty,
+                    'trf_in_qty' => null,
+                    'oth_in_qty' => null,
+                    'sale_qty' => null,
+                    'trf_out_qty' => null,
+                    'oth_out_qty' => null,
+                    'bal_qty' => null,
+                    'cost_rate' => null,
+                    'sale_rate' => null,
+                    'mrp' => null,
+                    'expiry_date' => null,
+                    'ven_code' => null,
+                    'expiry_date' => null,
+                    'oth_loc_code' => null,
+                    'oth_batch_no' => null,
+                    'oth_ref_date' => null,
+                    'comp_code' => null,
+                    'sch_narration' => null
+                ]);
+
+                return Response::json(['success' => true]);
             }
             elseif($open_stock_count <> 0)
             {
                 $open_stock_exist_count = DB::table('open_stock')->where('item_code', '=', $request->item_code)->where('mrp', '=', $request->mrp)->where('sale_rate', '=', $request->sale_rate)->where('cost_rate', '=', $request->cost_rate)->count();
-                if($open_stock_exist_count > 0)
-                {
 
+                if($open_stock_exist_count <> 0)
+                {
+                    $getQty=open_stock::select('qty')->where('item_code', '=', $request->item_code)->where('mrp', '=', $request->mrp)->where('sale_rate', '=', $request->sale_rate)->where('cost_rate', '=', $request->cost_rate)->first();
+				    $getNewQty = $getQty->qty + $request->qty;
+                    
+                    $open_stock_update = DB::table('open_stock')
+                                ->where('item_code', $request->item_code)
+                                ->where('mrp', $request->mrp)
+                                ->where('sale_rate', $request->sale_rate)
+                                ->where('cost_rate', $request->cost_rate)
+                                ->update(['qty' => $getNewQty]);
+                    
+                    $stock_detail_update = DB::table('stock_detail')
+                                ->where('item_code', $request->item_code)
+                                ->where('mrp', $request->mrp)
+                                ->where('sale_rate', $request->sale_rate)
+                                ->where('cost_rate', $request->cost_rate)
+                                ->update(['recd_qty' => $getNewQty]);
+                    
+                    if($open_stock_update and $stock_detail_update)
+                    {
+                        return Response::json(['success' => true]);
+                    }
+                    else{
+                        return Response::json(['error' => true]);
+                    }
                 }
                 elseif($open_stock_exist_count==0)
                 {
@@ -126,14 +184,45 @@ class openStock extends Controller
                         'dept_code' => $request->dept_code,
                         'expiry_date' => $request->expiry_date,
                         'batch_no' => $batch_post,
-                        'doc_type' => '',
+                        'doc_type' => 'OB',
                         'comp_code' => Session::get('companycode'),
                         'status' => 'Y',
                         'created_by' => Session::get('useremail')
                     ]);
+
+                    stock_detail::create([
+                        'loc_Code' => Session::get('companyloc_code'),
+                        'batch_no' => $batch_post,
+                        'item_code' => $request->item_code,
+                        'dept_code' => $request->dept_code,
+                        'doc_type' => 'OB',
+                        'recd_date' => null,
+                        'recd_qty' => $request->qty,
+                        'trf_in_qty' => null,
+                        'oth_in_qty' => null,
+                        'sale_qty' => null,
+                        'trf_out_qty' => null,
+                        'oth_out_qty' => null,
+                        'bal_qty' => null,
+                        'cost_rate' => null,
+                        'sale_rate' => null,
+                        'mrp' => null,
+                        'expiry_date' => null,
+                        'ven_code' => null,
+                        'expiry_date' => null,
+                        'oth_loc_code' => null,
+                        'oth_batch_no' => null,
+                        'oth_ref_date' => null,
+                        'comp_code' => null,
+                        'sch_narration' => null
+                    ]);
+
+                    return Response::json(['success' => true]);
                 }
             }
-            return Response::json(['success' => true]);
+        }
+        else{
+            return Response::json(['errors' => true]);
         }
     }
 
--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -233,7 +234,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
-
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
